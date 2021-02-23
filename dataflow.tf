527124bffc97e9774399c4a054a10bb8962608a7
samjithin <samjithin@gmail.com>
2021-02-23 15:53:34 +0530

    terraform fix2

--- dataflow.tf
+++ dataflow.tf
@@ -1,5 +1,6 @@
 resource "google_dataflow_job" "big_data_job" {
   name              = "dataflow-job"
+  zone = "us-central1-a"
   template_gcs_path = "gs://jit/templates/template-name.json"
   temp_gcs_location = "gs://jit/tmp"
   parameters = {
