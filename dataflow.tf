5844285469f2b41b7624733e15c5d48a6816e7a2
samjithin <samjithin@gmail.com>
2021-02-23 12:14:42 +0530

    added terraform with changes

--- dataflow.tf
+++ dataflow.tf
@@ -6,17 +6,13 @@ variable "region" {
   description = "region"
 }
 
-provider "google" {
-  credentials = file("creds.json")
-  project = var.project_id
-  region  = var.region
-}
+
 
 
 resource "google_dataflow_job" "big_data_job" {
   name              = "dataflow-job"
-  template_gcs_path = "gs://dataflow-templates/latest/Word_Count"
-  temp_gcs_location = "gs://jit/output/tmp"
+  template_gcs_path = "gs://jit/templates/template-name.json"
+  temp_gcs_location = "gs://jit/tmp"
   parameters = {
     inputFile="gs://dataflow-samples/shakespeare/kinglear.txt"
     output="gs://jit/output/"
