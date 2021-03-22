bfc894c17da4b3e06ec307fe5ea3c359495a7dfb
samjithin <56908165+samjithin@users.noreply.github.com>
2021-03-22 20:40:41 +0530

    Update dataflow.tf

--- dataflow.tf
+++ dataflow.tf
@@ -3,6 +3,9 @@ resource "google_dataflow_job" "big_data_job" {
   zone = "us-central1-a"
   template_gcs_path = "gs://jit/templates/template-name.json"
   temp_gcs_location = "gs://jit/tmp"
+  service_account_email = "dataflow@terraformlearning-284414.iam.gserviceaccount.com"
+  
+ 
   parameters = {
     inputFile="gs://dataflow-samples/shakespeare/kinglear.txt"
     output="gs://jit/output/"
